f6ee65a9c921f2b76b88e16b1efa1acaa0ad9d00
Curtis Vanzandt <ctvanzandt42@gmail.com>
2020-04-22 18:01:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1>The Office Quote Generator</h1>
+
+A CRUD application built with Java that handles quotes from NBC's "The Office".
+
+Technologies used:
+
+<ul>
+<li>Java 11</li>
+<li>PostgreSQL</li>
+<li>Spring Boot</li>
+</ul>
---
+++ src/test/java/com/curtisvanzandt/officequotegenerator/OfficeQuoteGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curtisvanzandt.officequotegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeQuoteGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
