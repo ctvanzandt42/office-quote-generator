ad90aae1c76c0411e40f5abb310b5a2f2b7c512a
Curtis Vanzandt <ctvanzandt42@gmail.com>
2020-04-22 19:52:06 -0500

    Added GetController and unit tests, also updated main test

---
+++ src/test/java/com/curtisvanzandt/officequotegenerator/OfficeQuoteGeneratorApplicationUnitTest.java
@@ -0,0 +1,14 @@
+package com.curtisvanzandt.officequotegenerator;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OfficeQuoteGeneratorApplicationUnitTest {
+
+    @Test
+    public void contextLoads() {
+        OfficeQuoteGeneratorApplication.main(new String[]{});
+    }
+
+}
